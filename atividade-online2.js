466ad5c6c60004ebc3e90af52cca8bac24b3cfd2
JenniferFreire <jennifer.freire@outlook.com>
2021-09-13 17:36:11 -0300

    atualizando atividade-online2

--- atividade-online2.js
+++ atividade-online2.js
@@ -1,57 +1,89 @@
 //* ------ SISTEMA DE CADASTRO DE EVENTOS ------- *//
 
-// o programa tem como objetivo o cadastramento de eventos de forma simples
-// solicitando:
-// nome do evento
-// data do evento
-// idade do palestrante, sendo que, apenas > 18 podem cadastrar um evento
-// lista de palestrante(S)
-// lista de participantes, sendo que, apenas podem ser listas até 100 participantes
+// O programa tem como objetivo o cadastramento de eventos de forma simples
+// Solicitando:
+// - nome do evento
+// - data do evento
+// - idade do palestrante, sendo que, apenas > 18 podem cadastrar um evento
+// - lista de palestrante(S)
+// - lista de participantes, sendo que, apenas podem ser listas até 100 participantes
 
 ////     VARIÁVEIS    ////
 
 let idade = 22;
-let data = new Date(15/09/2021);
-let datadoevento = 22/09/2021;
-let listadepart = ["Participante 1"," Participante 2"," Participante 3"," Participante 4"];
-let nomeevento = 'Como a linguagem Python máximiza seu trabalho?';
-let listadepale = ["Palestrante 1", " Palestrante 2"," Palestrante 3"];
+let datadoevento = (22+'/'+09+'/'+2021);
+let listadepart = ["Participante 1"," Participante 2"," Participante 3"," Participante 4"," Participante 5"," Participante 6"," Participante 7"," Participante 8"];
+let nomeevento = '"Como a linguagem Python máximiza seu trabalho?"';
+let listadepale = ["Palestrante 1", " Palestrante 2"];
+
+////      VÁRIAVEIS DATA   ////
+
+let data = new Date();
+let dia = data.getDate();                         // 1-31
+let mes = data.getMonth();                        // 0-11 (zero=janeiro)
+let ano4 = data.getFullYear();                    // 4 dígitos
+let str_data = dia + '/' + (mes) + '/' + ano4;
+
 
 ////     COMANDOS      ////
 
 // Solicitando o NOME do evento
 
-
 console.log(`Digite o nome do evento: ` + nomeevento);
 
-// Solicitando DATA do evento
+// ----------------- Solicitando DATA do evento ------------------ //
 
 console.log(`Digite a data do evento: ` + datadoevento);
-    if (datadoevento >= data) {
-        console.log(`Evento agendado.`)
-            } else {
-                console.log(`Data inválida.`)
-            }
 
-// Solicitando valor IDADE
+let evento = true == datadoevento >= str_data
 
-console.log(`Digite sua idade: ` + idade);      
-    if (idade >= 18){
+switch (evento) {
+    case true:
+    console.log(`Evento agendado.`)
+        break;
+    default: 
+    console.log(`Data inválida.`)  
+        return console.log(`Digite outra data para o evento:`)
+        break;
+}        
+
+// ------------------- Solicitando valor IDADE ------------------ //
+
+
+
+console.log(`Digite sua idade: ` + idade);
+
+let idade1 = true == idade >= 18;
+
+switch (idade1) {
+    case true:
         console.log(`Prosseguir com cadastro.`)
-            } else {
-                console.log(`Idade insuficiente. Cadastro não permitido.`)
-            }
-// Solicitando nome do PALESTRANTE     
+        break;  
+    default:
+        console.log(`Idade insuficiente. Cadastro não permitido.`)
+        return
+        break;
+}
+    
+// ------------ Solicitando nome do PALESTRANTE ----------------- //     
 
 console.log(`Insira o nome do(s) palestrante(s): `);
+
     console.log(`Palestrantes do evento: ` + listadepale)
 
-// Solicitando nome do PARTICIPANTE 
+// ----------- Solicitando nome do PARTICIPANTE ----------------- // 
 
-console.log(`Insira o nome dos participantes: `);
-    console.log(`Participantes do evento: `+ listadepart)
-        if (listadepart.length <= 100){
+console.log(`Insira o nome dos participantes:`);
+
+    console.log(`Participantes do evento: ` + listadepart);
+
+let participantes = true == listadepart.length <= 100;
+
+switch (participantes) {
+    case true:
         console.log(`Cadastro realizado com sucesso.`)
-            } else {
-                console.log(`Cadastro não permitido. Limite de participantes exedido.`)
-            }
+        break;  
+    default:
+        console.log(`Cadastro não permitido. Limite de participantes exedido.`)
+        break;
+}
