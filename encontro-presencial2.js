08dbfe24103bfe4d34642d78da2bf0bb2ebdde9c
Jennifer Freire <88242134+jenniferfreire@users.noreply.github.com>
2021-09-13 19:35:29 -0300

    Update encontro-presencial2.js

--- encontro-presencial2.js
+++ encontro-presencial2.js
@@ -4,16 +4,16 @@
 const listadealunos = 8;
 //   FOR está contando do index (listadealunos) começando do zero,
 //   limitado a contar a quantidade existente no index, adicioando + 1 
-//   para percorrer toda a "lista".
+//   para percorrer toda a numeração.
 for (let index = 0; index <= listadealunos; index++) {
-//  SE index for totalmente igual (===) a zero, imprimirá: "Número é zero".
-  if (index === 0) 
+//  SE index for igual (==) a zero, imprimirá: "Número é zero".
+  if (index == 0) 
    {
     console.log(index + ` : Número zero`);
-// E SE o resto da divisão (%) do index por 2 for totalmente igual (===) a zero, imprimirá: "Número par".
+// E SE o resto da divisão (%), do index, por 2, for igual (==) a zero, imprimirá: "Número par".
    } else if  (index % 2 === 0){
         console.log(index + ` : Número par`);    
-// SE NÃO imprimirá: "Número impar", pois seu resto da divisão (%) por 2 não é zero.
+// SE NÃO imprimirá: "Número impar", pois seu resto da divisão (%), por 2, não é zero.
     } else {
             console.log(index + ` : Número ímpar`);
     }
