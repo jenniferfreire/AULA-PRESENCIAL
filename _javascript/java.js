5cc015ade426e21cafc045d62c13132402ecc9d6
JenniferFreire <jennifer.freire@outlook.com>
2021-10-30 15:03:50 -0300

    subindo arquivos 30/10

---
+++ _javascript/java.js
@@ -0,0 +1,12 @@
+// declaração de variavel
+
+var nome
+
+// abrir uma tela no navegador
+nome = window.prompt("Digite o seu nome:")
+
+// imprimir o que foi escrito acima
+document.write("Bom dia, "+ nome + "!<br>")
+
+//
+document.write("tchau!");
---
+++ _javascript/java2.js
@@ -0,0 +1,9 @@
+// caixa de texto
+function mudou1()
+{
+    document.formulário.nome_completo.value=document.formulário.nome_completo.value + " " + document.formulário.nome.value
+}
+function mudou2()
+{
+    document.formulário.nome_completo.value=document.formulário.nome_completo.value + " " + document.formulário.sobrenome.value
+}
